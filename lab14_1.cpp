3501313275f61b5a85bd10b329d1c938543d1e4d
Daiyaz <uchihachikk3@gmail.com>
2025-01-24 17:13:20 +0700

    Implement insertion sort function and update main for sorting demonstration

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
+void insertionSort(T d[],int N);
 
-}
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
@@ -17,3 +17,28 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
+template <typename T>
+void insertionSort(T d[],int N){
+	for(int i=1;i<N;i++){
+		T key = d[i];
+		int j= i-1;
+
+
+		while(d[j]<key  && j>=0){
+			d[j+1] = d[j];
+			j--;
+		}
+	 d[j+1] =  key;
+	 
+	 cout << "Pass "<<i<<":";
+	 for(int k=0;k<N;k++){
+		cout << d[k]<<  " ";	
+	 }
+	 cout<<endl;
+	}
+		
+	
+
+}
+
